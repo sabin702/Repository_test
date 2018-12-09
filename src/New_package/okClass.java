142cbb0c79fb53d3c63308b80bc90de843a08621
Sabin <35765016+xplameboy@users.noreply.github.com>
2018-12-09 14:50:26 +0100

    I've created the ok class
    This is a new class that I've just created

---
+++ src/New_package/okClass.java
@@ -0,0 +1,13 @@
+package New_package;
+
+public class okClass
+{
+   public static void main(String[] args)
+   {
+      boolean ok = true;
+      if (ok)
+         System.out.println("True");
+      else
+         System.out.println("False");
+   }
+}
